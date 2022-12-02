9278e386025ffb16b4c201e206b357ca22385794
Walter Guimaraes Junior <walterguimaraesjr@gmail.com>
2022-12-02 12:35:49 +0000

    Add files via upload

---
+++ New folder/DAL/DAO/UserDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DAL.DAO
+{
+    public class UserDAO
+    {
+        //public tb_usuario ValidarLogin(string email_usuario, string senha_usuario)
+        //{
+        //    Banco objBanco = new Banco();
+
+        //    tb_usuario objUser = objBanco.tb_usuario.FirstOrDefault(p => p.email_usuario == email_usuario && p.senha_usuario == senha_usuario);
+        //    return objUser;
+        //}
+    }
+}
